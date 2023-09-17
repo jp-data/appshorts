17a4a5a4e71af4c8bb3ed43ea2ffc3efaad469a5
joao <joaopaulo94.economia@gmail.com>
2023-09-17 15:36:01 -0300

    Comentários - função convert.js

--- server/convert.js
+++ server/convert.js
@@ -1,38 +1,54 @@
 // manipulação de arquivos
 import fs from 'fs'
+// pacote de conversão do áudio
 import wav from 'node-wav'
+// pacote para manipular o áudio
 import ffmpeg from 'fluent-ffmpeg'
+// pacote para manipular o arquivo
 import ffmpegStatic from 'ffmpeg-static'
 
+// local do arquivo que será convertido
 const filePath = "./tmp/audio.mp4"
+// local de armazenamento do arq convertido
 const outputPath = filePath.replace('.mp4', ".wav")
 
 
 //função que converte o vídeo
 export const convert = () => new Promise ((resolve, reject) => {
   console.log("Convertendo o vídeo...")
-
+  //
   ffmpeg.setFfmpegPath(ffmpegStatic)
+  // indicando o local do arquivo que será manipulado
   ffmpeg()
   .input(filePath)
+  // frequencia de áudio utilizada
   .audioFrequency(16000)
+  // número de canais
   .audioChannels(1)
+  // definindo o formato
   .format("wav")
-  .on("end", () => {
-    const file = fs.readFileSync(outputPath)
-    const fileDecoded = wav.decode(file)
 
+  .on("end", () => {
+    //lendo o arquivo convertido
+    const file = fs.readFileSync(outputPath)
+    // decodificando o arquivo
+    const fileDecoded = wav.decode(file)
+    // captando o áudio no índice 0
     const audioData = fileDecoded.channelData[0]
+    // convertendo no formato que a I.A utiliza
     const floatArray = new float32Array(audioData)
 
     console.log("Vídeo convertido com sucesso")
 
     resolve(floatArray)
+    // deletando o arquivo da pasta após a conversão
     fs.unlinkSync(outputPath)
   })
+  // captando erro
   .on("error", (error) => {
     console.log("Erro ao converte vídeo", error)
     reject(error)
   })
+  // caso não haja erro, salva o arquivo
   .save(outputPath)
 })
--- server/index.js
+++ server/index.js
@@ -13,10 +13,13 @@ app.use(cors())
 
 //criando a rota que recupera o ID do vídeo e usa a função que realiza o download
 app.get('/summary/:id', async(req, res) => {
+  // download do vídeo
   await download(req.params.id)
+  // conversão do vídeo
   const audioConverted = await convert()
   console.log(audioConverted)
-  const result = await transcribe() 
+  // transcrevendo o áudio convertido
+  const result = await transcribe(audioConverted) 
 
   return res.json({ result })
 })
