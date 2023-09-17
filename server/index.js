a09a67846be334c560a8825958e0a99437b60cba
joao <joaopaulo94.economia@gmail.com>
2023-09-17 15:55:30 -0300

    criando a  função que transcreve o áudio do vídeo - modelo I.A

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.use(cors())
 
 //criando a rota que recupera o ID do vídeo e usa a função que realiza o download
 app.get('/summary/:id', async(req, res) => {
+  
+  try{
   // download do vídeo
   await download(req.params.id)
   // conversão do vídeo
@@ -22,12 +24,21 @@ app.get('/summary/:id', async(req, res) => {
   const result = await transcribe(audioConverted) 
 
   return res.json({ result })
+  } catch(error){
+    console.log(error)
+    return res.json({ error })
+  }
 })
 
 // rota que recupera a URL do vídeo no forms
 app.post("/summary", async (req, res) => {
+  try{
   const result = await summarize(req.body.text)
   return res.json({ result })
+  } catch(error){
+    console.log(error)
+    return res.json({ error })
+  }
 })
 
 //definindo o número da porta
--- server/transcribe.js
+++ server/transcribe.js
@@ -1,4 +1,29 @@
-import { transcriptionExample } from "./utils/transcription.js"
-export async function transcribe() {
-  return transcriptionExample
+// importando o modelo de I.A que transcreve o áudio
+import { pipeline } from "@xenova/transformers"
+
+// import { transcriptionExample } from "./utils/transcription.js"
+// função para extrair o texto
+export async function transcribe(audio) {
+  
+  // aplicando o modelo no áudio convertido
+  try {
+    console.log("Realizando a transcrição")
+    const transcribe = await pipeline
+    ("automatic-speech-recognition",
+     "xenova/whisper-small")
+
+     const transcription = await transcribe(audio, {
+      //dividindo o conteúdo e definindo a tarefa do modelo
+      chunk_length_s: 30,
+      stride_length_s: 5,
+      language: "portuguese",
+      task: "transcribe",
+     })
+
+     console.log('Transcrição finalizada com sucesso.')
+     //retornando o conteúdo da transcrição e retirando a música da transcrição dos vídeos
+     return transcription?.text.replace('[Música]', '')
+  } catch (error) {
+    throw new Error(error)
+  }
 }
